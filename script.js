871fab8a5d775a38dcfad1d35a8d7024f92d10fd
Maruf <maruf@maruf.maruf>
2020-11-04 23:24:19 +0500

    allDone

---
+++ script.js
@@ -0,0 +1,51 @@
+function receipt() {
+    const arrayReceipt = [{
+        'Lavash': {
+            info: 'Big Cheese',
+            price: 15000
+        }, 'Hot-dog': {
+            info: 'mini',
+            price: 8500
+        }, 'Cola': {
+            info: '1.5 L',
+            price: 9000
+        }
+    }, {
+        'Milk': {
+            info: '1 L',
+            price: 8500
+        },
+        'Sugar': {
+            info: '1 kg',
+            price: 7000
+        }
+    }, {
+        'Cola': {
+            info: '1 L',
+            price: 6500
+        },
+        'Fanta': {
+            info: '1.5 L',
+            price: 9000
+        },
+        'Lipton': {
+            info: '1.2 L',
+            price: 5000
+        }, 
+        'Nestle': {
+            info: '10 L',
+            price: 8500
+        }
+    }]
+    const count = Math.floor(Math.random() * arrayReceipt.length)
+    return arrayReceipt[count]
+}
+
+let a = receipt();
+let allPrice = 0;
+
+for (const key in a) {
+        console.log(`Вы заказали: ${key} ${a[key]["info"]} , цена: ${a[key]["price"]}`);
+    var b = `Общая цена: ${allPrice += a[key]["price"]}`;
+}
+console.log(b);
